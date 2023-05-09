bf0088269d37f3cfebef52bc79278d2e3c6781cb
Bikramjeet1998 <bs863313@gmail.com>
2023-05-09 02:21:41 -0500

    Auth done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\usertable;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -18,13 +21,43 @@ class UserController extends Controller
         return view('create');
     }
 
+    public function login(Request $request){
+        
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $isLogin = Auth::attempt($validated);
+
+        if($isLogin){
+
+            $user = Auth::user();
+
+            if($user->role_id === 1 ){
+                    // redirect to admin
+                    //admin/dashboard
+            }else{
+                // redirect to user
+                //user.dashboard
+            }
+            dd();
+            //redirect where you want to go after login
+        }
+
+
+        return back()->withErrors([
+            'error' => "Credentials not match"
+        ]);
+    }
+
 
     public function store(LoginRequest $request)
     {
-        $userdata = new usertable;
+        $userdata = new User;
         $userdata->name = $request->Username;
         $userdata->email = $request->Email;
-        $userdata->password = $request->Password;
+        $userdata->password = Hash::make($request->Password);
         $userdata->phone = $request->Phone;
         $userdata->status = $request->Status;
         $userdata->save();
